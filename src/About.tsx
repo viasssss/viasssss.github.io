c869af50d1db6fbda306c570959ccb6d686b8589
viasssss <87640014+viasssss@users.noreply.github.com>
2025-08-31 21:57:32 +1200

    update about page

--- src/About.tsx
+++ src/About.tsx
@@ -1,6 +1,12 @@
+import Button from 'react-bootstrap/Button'
+
 function About(){
     return (
+        <>
         <h2>My name is Riley.</h2>
+        <Button variant="outline-primary" as='input' 
+        type='submit' value='Submit' size='lg'></Button>
+        </>
     )
 }
 
